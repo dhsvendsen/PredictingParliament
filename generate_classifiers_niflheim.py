6c85c52025e970f5944698b0d0d29af7ed1ea2a4
Ulf Aslak Jensen <ulfaslak@Ulfs-MacBook-Pro.local>
2014-11-30 10:49:34 +0100

    Changes to OdaGetter and routine for storing classifiers

--- generate_classifiers_niflheim.py
+++ generate_classifiers_niflheim.py
@@ -17,11 +17,15 @@ all_mp = [dicts['id'] for dicts in OdaParser.all_MPs()]
 DecisionTreeClassifier(max_depth=5)
 
 for mp_id in all_mp:
-    X,y = classifier_data.dataset_X_y(mp_id)
-    np.save('X_{0}'.format(mp_id),X)
-    np.save('y_{0}'.format(mp_id),y)
-    #dtc = DecisionTreeClassifier(max_depth=5).fit(X,y)
-    #with open('classifier_{0}.pkl'.format(mp_id), 'wb') as out_file:
-    #    pickle.dump(dtc, out_file) 
+	try:
+		X,y = classifier_data.dataset_X_y(mp_id)
+	except:
+		print "Failed to retrieve dataset for politician with ID:", mp_id
+		continue
+	np.save('X_{0}'.format(mp_id),X)
+	np.save('y_{0}'.format(mp_id),y)
+	#dtc = DecisionTreeClassifier(max_depth=5).fit(X,y)
+	#with open('classifier_{0}.pkl'.format(mp_id), 'wb') as out_file:
+	#    pickle.dump(dtc, out_file) 
     
 
