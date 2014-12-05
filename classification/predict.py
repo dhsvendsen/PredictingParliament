91c8d09936a4bd7410557bd2592ddd35905dafb8
Daniel Svendsen <d.h.svendsen@gmail.com>
2014-12-05 18:22:52 +0100

    More test and predict

--- classification/predict.py
+++ classification/predict.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
-import sys; import os
-sys.path.insert(0, os.path.abspath('..'))
+import os
+PARENTDIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+os.sys.path.insert(0, PARENTDIR)
 
 import dataretrieval.odaparsers as opa
 import numpy as np
@@ -9,16 +10,13 @@ from sklearn.tree import DecisionTreeClassifier
 import pickle
 
 all_mp = [(dicts['id'],dicts['navn']) for dicts in opa.all_MPs()]
-
-DecisionTreeClassifier(max_depth=5)
-
 predictions = []
-
 A = np.zeros(52)
 
 for mp in all_mp:
     try:
-        with open('decisiontrees/classifier_{0}.pkl'.format(mp[0]), 'rb') as in_file:
+        with open(PARENTDIR + '/storing/decisiontrees/classifier_%d.pkl' % mp,
+                  'rb') as in_file:
             dtc = pickle.load(in_file)
         predictions.append((mp[1], dtc.predict(A)[0]))
     except IOError:
--- test/pytest_dataretrieval.py
+++ test/pytest_dataretrieval.py
@@ -5,12 +5,14 @@ parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 os.sys.path.insert(0, parentdir)
 
 import numpy as np
-from dataretrieval import odagetter, odaparsers
+npt = np.testing
+import dataretrieval.odagetter as oget
+import dataretrieval.odaparsers as opa
 
+# Tests for odagetter
+GETTER = oget.OdaGetter(?!?!?!)
 
 def test_feat_party():
-    """Test if the relevant case can be found and the right feature array
-    extracted
-    """
-    assert (cda.feat_party(69) == np.array(
-    [ 0.,  0.,  0.,  0.,  1.,  0.,  0.,  0.])).all()
+    
+    
+# Tests for odagetter
