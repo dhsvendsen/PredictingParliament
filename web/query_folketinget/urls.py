11142003dedd17871a8ab304478c6b307ba53034
ulfaslak <ulfjensen@gmail.com>
2014-12-05 17:56:28 +0100

    del web

--- web/query_folketinget/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, url
-
-from query_folketinget import views
-
-urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
-    url(r'^predict', views.predict, name='predict'),
-)
